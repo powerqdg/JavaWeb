68ea510e1004c5e1e440b840aa7451f35c4358c0
dgshin <powerqdg@gmail.com>
2023-12-08 13:10:55 +0900

    @WebServlet 애노테이션 적용

--- src/lesson01/servlet/CalculatorServlet.java
+++ src/lesson01/servlet/CalculatorServlet.java
@@ -7,7 +7,9 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet("/calc")
 public class CalculatorServlet extends GenericServlet {
 	private static final long serialVersionUID = 1L;
 
--- src/lesson01/servlet/HelloWorld2.java
+++ src/lesson01/servlet/HelloWorld2.java
@@ -6,7 +6,9 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet("/hello2")
 public class HelloWorld2 extends GenericServlet {
 	private static final long serialVersionUID = 1L;
 
