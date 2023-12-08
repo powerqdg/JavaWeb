3d27e3b6da450fa99b91d772ed7d805d9f6d259c
dgshin <powerqdg@gmail.com>
2023-12-08 14:00:52 +0900

    MemberListServlet 생성

---
+++ src/lesson02/servlets/MemberListServlet.java
@@ -0,0 +1,57 @@
+package lesson02.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/member/list")
+public class MemberListServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "scott", "tiger");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT MNO, EMAIL, PWD, MNAME, CRE_DATE, MOD_DATE FROM MEMBERS ORDER BY MNO");
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>회원목록</title></head>");
+			out.println("<body><h1>회원목록</h1>");
+			out.println("<ul>");
+			while (rs.next()) {
+				out.println("<li>" + 
+						rs.getInt("MNO") + ", " + 
+						rs.getString("MNAME") + ", " + 
+						rs.getString("EMAIL") + ", " +
+						rs.getDate("CRE_DATE") + ", " +
+						rs.getDate("MOD_DATE") + "<br>" +
+						"</li>");
+			}
+			out.println("</ul>");
+			out.println("</body></html>");
+			
+		} catch (Exception e) {
+			throw new ServletException(e);
+		} finally {
+			try {if (rs != null) rs.close();} catch (Exception e) {}
+			try {if (stmt != null) stmt.close();} catch (Exception e) {}
+			try {if (conn != null) conn.close();} catch (Exception e) {}
+		}
+	}
+
+}
