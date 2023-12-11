c40d85d306469b7e3deafc8a7ad7f1e0d614fca5
dgshin <powerqdg@gmail.com>
2023-12-11 10:01:14 +0900

    서버에서 제공하는 DataSource 적용

--- src/lesson02/listener/ContextLoaderListener.java
+++ src/lesson02/listener/ContextLoaderListener.java
@@ -1,26 +1,21 @@
 package lesson02.listener;
 
+import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import javax.sql.DataSource;
 
 import lesson02.dao.MemberDao;
 
 public class ContextLoaderListener implements ServletContextListener {
-	BasicDataSource ds;
-	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		try {
 			ServletContext sc = sce.getServletContext();
 			
-			ds = new BasicDataSource();
-			ds.setDriverClassName(sc.getInitParameter("driver"));
-			ds.setUrl(sc.getInitParameter("url"));
-			ds.setUsername(sc.getInitParameter("username"));
-			ds.setPassword(sc.getInitParameter("password"));
+			InitialContext initialContext = new InitialContext();
+			DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/orcl");
 			
 			MemberDao memberDao = new MemberDao();
 			memberDao.setDataSource(ds);
@@ -32,7 +27,5 @@ public class ContextLoaderListener implements ServletContextListener {
 	}
 	
 	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		try {if (ds != null)  ds.close();} catch (Exception e) {}
-	}
+	public void contextDestroyed(ServletContextEvent sce) {}
 }
